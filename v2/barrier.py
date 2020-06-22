f978693262c534fa6e2f05bb8a9580c65c1b0934
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-06-22 23:59:38 +0200

    Create v2 from new v3

--- v2/barrier.py
+++ v2/barrier.py
@@ -1,9 +1,12 @@
-def is_number(x):
-    return any([isinstance(x, _type) for _type in (int, float)])
+
+# Engine
+from engine.helpers import is_number, merge_into_list_dict, reduce_to_relevant_collisions, get_collision
+from engine.tests import TEST_mandatory_coordinates
 
 
 class SquareBarrier:
 
+    # A list of all SquareBarrier instances
     instances = []
 
     def __init__(
@@ -14,23 +17,11 @@ class SquareBarrier:
             color=(150, 150, 150)
     ):
 
-        # Just some tests to validate that the correct init_values are given
-        assert \
-            x_left is None and x_center is not None or \
-            x_center is None and x_left is not None, \
-            "Exactly one of (x_left, x_center) has to be set"
-        assert \
-            is_number(x_left if x_left is not None else x_center),\
-            "x has to be a number (integer or float)"
-        assert \
-            y_top is None and y_center is not None or \
-            y_center is None and y_top is not None, \
-            "Exactly one of (y_top, y_center) has to be set"
-        assert \
-            is_number(y_top if y_top is not None else y_center), \
-            "y has to be a number (integer or float)"
+        TEST_mandatory_coordinates(x_left=x_left, x_center=x_center, y_top=y_top, y_center=y_center)
 
         # self.position is referring to the blocks center
+        # All properties as lists with length 2
+        # => [x-component, y-component]
         self.position = [
             (x_left + (width/2)) if x_left is not None else x_center,
             (y_top - (height/2)) if y_top is not None else y_center
@@ -38,39 +29,15 @@ class SquareBarrier:
         self.size = [width, height]
         self.color = color
 
+        # Add this new instances to the instance-list from above
         SquareBarrier.instances.append(self)
 
+    # Draw all SquareBarrier instances
     @staticmethod
     def draw_all(game):
         for barrier in SquareBarrier.instances:
+            # Uses the scaled draw rect method from engine.game
             game.draw_rect_element(barrier.position, barrier.size, color=barrier.color)
-
-    def detect_collision(self, player):
-        dx_min = player.size[0]/2 + self.size[0]/2
-        dy_min = player.size[1]/2 + self.size[1]/2
-
-        dx = player.position[0] - self.position[0]  # dx > 0 = player is right from the barrier
-        dy = player.position[1] - self.position[1]  # dy > 0 = player is above the barrier
-
-        horizontal_overlap = (dx_min - abs(dx))
-        vertical_overlap = (dy_min - abs(dy))
-
-        collision = {}
-
-        if vertical_overlap > 0 and horizontal_overlap > 0:
-
-            if vertical_overlap < horizontal_overlap:
-                if dy > 0:
-                    collision = {"FLOOR": self.position[1] + self.size[1]/2}
-                else:
-                    collision = {"CEILING": self.position[1] - self.size[1]/2}
-            else:
-                if dx > 0:
-                    collision = {"LEFT_WALL": self.position[0] + self.size[0]/2}
-                else:
-                    collision = {"RIGHT_WALL": self.position[0] - self.size[0]/2}
-
-        return collision
 
     @staticmethod
     def detect_all_collisions(player):
@@ -82,19 +49,12 @@ class SquareBarrier:
             'RIGHT_WALL': [],
         }
         for barrier in SquareBarrier.instances:
-            single_collision = barrier.detect_collision(player)
-            for side in single_collision:
-                all_collisions[side].append(single_collision[side])
+            all_collisions = merge_into_list_dict(
+                all_collisions,
+                get_collision(barrier=barrier, moving_object=player)
+            )
 
         # 2. Reduce all collisions to the relevant ones, example:
         #    all_collisions['FLOOR'] = [3.0, 4.2, 2.2, 4.0]
         #    -> relevant_collisions['FLOOR'] = 4.2
-        relevant_collisions = {}
-        for side in all_collisions:
-            collisions_count = len(all_collisions[side])
-            if collisions_count > 0:
-                if collisions_count > 1:
-                    all_collisions[side] = list(sorted(all_collisions[side], reverse=side in ('FLOOR', 'LEFT_WALL')))
-                relevant_collisions[side] = all_collisions[side][0]
-
-        return relevant_collisions
+        return reduce_to_relevant_collisions(all_collisions)
--- v2/player.py
+++ v2/player.py
@@ -1,11 +1,19 @@
-from barrier import SquareBarrier
 
+# Engine
+from engine.helpers import merge_into_list_dict, reduce_to_relevant_collisions, get_collision
+from engine.tests import *
+
+# Constants
 from pygame.constants import *
-from constants import *
+from engine.constants import *
+
+# Components
+from v2.barrier import SquareBarrier
+
 
 class Player:
 
-    # Keeps track of all instances of the Player class
+    # A list of all Player instances
     instances = []
 
     def __init__(
@@ -25,22 +33,20 @@ class Player:
                 K_RIGHT: 'RIGHT'
             }
         else:
-            # Just a few tests whether the passed keymap is valid
-            assert \
-                all([value in ('UP', 'LEFT', 'DOWN', 'RIGHT') for value in keymap.values()]), \
-                'All keys [UP, LEFT, RIGHT] required in keymap property'
+            TEST_keymap(keymap)
 
-            assert \
-                all([isinstance(key, int) for key in keymap.keys()]), \
-                'All keys for [UP, LEFT, DOWN, RIGHT] in keymap property have to be integers (e.g. pygame.K_UP)'
-
-            assert len(keymap) == 4, 'Only keys for [UP, LEFT, DOWN, RIGHT] allowed in keymap property'
-
-        self.position = position
+        # All properties as lists with length 2
+        # => [x-component, y-component]
+        self.starting_position = [p for p in position]
+        self.position = [p for p in position]
         self.size = [width, height]
         self.velocity = [0.0, 0.0]
 
+        # Store the relations between keyboard keys and
+        # movement direction i.e. WASD vs ULDR
         self.keymap = keymap
+
+        # Store which keys are currently being pressed
         self.keypressed = {
             'UP': False,
             'LEFT': False,
@@ -51,6 +57,7 @@ class Player:
         self.name = name
         self.color = color
 
+        # The collisions that are currently being detected
         self.collisions = {
             'CEILING': None,
             'FLOOR': None,
@@ -58,17 +65,35 @@ class Player:
             'RIGHT_WALL': None
         }
 
-        # Add this new instance to the instance-list Player.players
+        # Add this new instances to the instance-list from above
         Player.instances.append(self)
 
     def keypress(self, event_key, keydown):
-        assert \
-            event_key in self.keymap.keys(),\
-            f"Only key-events from {list(self.keymap.keys())} allowed"
+        # We know from self.keymap which event.key will lead to which
+        # direction in self.keypressed being set to true or false
+
+        # "keydown" is a boolean value and "event_key" is a constant
+        # like "pygame.K_LEFT"/"pygame.K_RIGHT"
+
+        TEST_keypress(self.keymap, event_key)
         self.keypressed[self.keymap[event_key]] = keydown
 
-    def update_for_collisions(self, new_velocity, new_position):
-        all_collisions = SquareBarrier.detect_all_collisions(self)
+    def update_for_collisions(self, new_velocity, new_position, timedelta):
+
+        # 1. Detecting movement collisions with Barriers and other
+        #    players. Collisions should refer to the new velocity
+        #    and new position that is why we preliminarily update
+        #    the players state (self.)
+        self.velocity, self.position = new_velocity, new_position
+        movement_collisions = reduce_to_relevant_collisions(
+            SquareBarrier.detect_all_collisions(self)
+        )
+
+        # The strategy now is to go through all the collisions that
+        # were detected and successively adjust new_velocity and
+        # new_position if needed. In the end the players state (self.
+        # will be updated with the adjusted position/velocity once
+        # again
 
         new_collisions = {
             'CEILING': None,
@@ -77,79 +102,92 @@ class Player:
             'RIGHT_WALL': None
         }
 
-        def fix_position(side):
+        # "Snap" to Wall/Floor/Ceiling when hitting one. Example
+        #   Moving left & new_x = -0.04
+        #   Wall detected at x=0
+        #   Snaps to x=0 and velocity *= -1 (turns around)
+        def snap_to_barrier(side):
             dimension = 1 if (side in ('FLOOR', 'CEILING')) else 0
             coordinate_direction = +1 if (side in ('FLOOR', 'LEFT_WALL')) else -1
             new_velocity[dimension] = 0
             limit_center_offset = ((self.size[dimension]/2) - ERROR_MARGIN) * coordinate_direction
-            new_position[dimension] = all_collisions[side] + limit_center_offset
-            new_collisions[side] = all_collisions[side]
+            new_position[dimension] = movement_collisions[side] + limit_center_offset
+            new_collisions[side] = movement_collisions[side]
 
-        if 'FLOOR' in all_collisions and new_velocity[1] < ERROR_MARGIN:
-            fix_position('FLOOR')
-        elif 'CEILING' in all_collisions and new_velocity[1] > -ERROR_MARGIN:
-            fix_position('CEILING')
+        # 2. React to vertical collisions
+        if movement_collisions['CEILING'] is not None:
+            if new_velocity[1] > -ERROR_MARGIN:
+                snap_to_barrier('CEILING')
+        elif movement_collisions['FLOOR'] is not None and new_velocity[1] < +ERROR_MARGIN:
+            snap_to_barrier('FLOOR')
 
-        if 'LEFT_WALL' in all_collisions and new_velocity[0] < ERROR_MARGIN:
-            fix_position('LEFT_WALL')
-        elif 'RIGHT_WALL' in all_collisions and new_velocity[0] > -ERROR_MARGIN:
-            fix_position('RIGHT_WALL')
+        # 3. React to horizontal collisions. Left and Right wall block
+        # is only applied if the player is moving towards that barrier
+        if movement_collisions['LEFT_WALL'] is not None and new_velocity[0] < ERROR_MARGIN:
+            snap_to_barrier('LEFT_WALL')
+        elif movement_collisions['RIGHT_WALL'] is not None and new_velocity[0] > -ERROR_MARGIN:
+            snap_to_barrier('RIGHT_WALL')
 
+        # 4. Now new_velocity and new_position has been adjusted to conform
+        #    with collisions -> player state will be updated with the adjusted
+        #    values
         self.collisions = new_collisions
         self.velocity = [round(v, COORDINATE_PRECISION) for v in new_velocity]
         self.position = [round(p, COORDINATE_PRECISION) for p in new_position]
 
+    # Update a single Player instances
     def update(self, timedelta):
-        new_velocity = [0, 0]
 
+        # Preliminary new velocity
+        new_velocity = [0.0, 0.0]
+
+        # Set current horizontal velocity according to
+        # self.collisions and self.keypressed
         if self.collisions['RIGHT_WALL'] is None:
             if self.keypressed['RIGHT'] and not self.keypressed['LEFT']:
-                new_velocity[0] = RUN_VELOCITY
-
+                new_velocity[0] = +RUN_VELOCITY
         if self.collisions['LEFT_WALL'] is None:
             if self.keypressed['LEFT'] and not self.keypressed['RIGHT']:
                 new_velocity[0] = -RUN_VELOCITY
 
+        # Set current vertical velocity according to
+        # self.collisions and self.keypressed
         if self.collisions['FLOOR'] is not None:
             if self.keypressed['UP'] and not self.keypressed['DOWN']:
                 if self.velocity[1] < ERROR_MARGIN:
                     new_velocity[1] = JUMP_VELOCITY
         else:
             if self.keypressed['DOWN']:
-                new_velocity[1] = -JUMP_VELOCITY  # A Mario like forced smash downwards
+                # A Mario like forced smash downwards
+                new_velocity[1] = -JUMP_VELOCITY
             else:
+                # Regular gravitational acceleration
                 new_velocity[1] = self.velocity[1] - GRAVITY * timedelta
 
+        # Preliminary new position
         new_position = [
             self.position[0] + new_velocity[0] * timedelta,
             self.position[1] + new_velocity[1] * timedelta
         ]
 
-        self.update_for_collisions(new_velocity, new_position)
+        # Adjust new_velocity and new_position by detecting collisions
+        # for the new state and modifying the new state to comply with
+        # the collisions with other enemies, players and barriers
+        self.update_for_collisions(new_velocity, new_position, timedelta)
 
+    # Update all Player instances
     @staticmethod
     def update_all(timedelta):
         for player in Player.instances:
             player.update(timedelta)
 
+    # Draw a single Player instances
     def draw(self, game):
-        game.draw_rect_element(self.position, self.size, color=self.color)
-
+        game.draw_rect_element(self.position, self.size, color=self.color, alpha=1.0)
         if DRAW_HELPERS:
-            circle_radius = min(self.size) * 0.1
-            circle_offsets = {
-                "FLOOR": [0, -0.5*self.size[1] / 2],
-                "CEILING": [0, 0.5*self.size[1] / 2],
-                "LEFT_WALL": [-0.5*self.size[0], 0],
-                "RIGHT_WALL": [0.5*self.size[0], 0],
-            }
+            game.draw_helper_points(self)
 
-            game.draw_circle_element(self.position, circle_radius, color=(0, 0, 255))
-            for side in circle_offsets:
-                if self.collisions[side] is not None:
-                    position = [self.position[dim] + circle_offsets[side][dim] for dim in (0, 1)]
-                    game.draw_circle_element(position, circle_radius, color=(0, 0, 255))
-
+    # Draw all Player instances
     @staticmethod
     def draw_all(game):
         for player in Player.instances:
